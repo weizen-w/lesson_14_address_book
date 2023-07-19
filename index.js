a9ac11a2808fb66dbdc07ddebb0ce63d0b47c9ed
Wladimir Weizen <weizen.w@gmx.de>
2023-07-19 18:27:46 +0200

    fix3 CW

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 const listUl = document.getElementById("list-ul");
-const imageElement = document.getElementById("foto");
-const nameElement = document.getElementById("name");
-const infoElement = document.getElementById("info");
-const addressElement = document.getElementById("address");
-const phoneElement = document.getElementById("phone");
 
 async function getContact() {
   const response = await fetch(
     "https://weizen-w.github.io/lesson_14_address_book/adress_book.json"
   );
-  const obj = await response.json();
-  const { foto, firstName, lastName, gender, age, address, phoneNumbers } = obj;
-  const { streetAddress, city, state, postalCode } = address;
-  const { type, number } = phoneNumbers;
-  imageElement.src = foto;
-  nameElement.textContent = `${firstName} ${lastName}`;
-  infoElement.textContent = `age: ${age}, gender: ${gender}`;
-  addressElement.textContent = `adress: ${state} - ${postalCode}, ${city}, ${streetAddress}`
-  phoneElement.textContent = `phone: ${number} - ${type}`;
+  const arrayJson = await response.json();
+  arrayJson.forEach(obj => {
+    const { foto, firstName, lastName, gender, age, address, phoneNumbers } = obj;
+    const { streetAddress, city, state, postalCode } = address;
+    const { type, number } = phoneNumbers;
+    const imageElement = document.createElement("img");
+    imageElement.src = foto;
+    imageElement.style.width = "200px";
+    const nameElement = document.createElement("h3");
+    nameElement.textContent = `${firstName} ${lastName}`;
+    const infoElement = document.createElement("p");
+    infoElement.textContent = `age: ${age}, gender: ${gender}`;
+    const addressElement = document.createElement("p");
+    addressElement.textContent = `adress: ${state} - ${postalCode}, ${city}, ${streetAddress}`;
+    const phoneElement = document.createElement("p");
+    phoneElement.textContent = `phone: ${number} - ${type}`;
+    const liElement = document.createElement("li");
+    liElement.append(imageElement, nameElement, infoElement, addressElement, phoneElement);
+    listUl.append(liElement);
+  });
 }
 
 getContact();
