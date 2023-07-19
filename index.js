f7ad9f1d5dfdad1c74c788d672d68c941d23a8c6
Wladimir Weizen <weizen.w@gmx.de>
2023-07-19 18:06:07 +0200

    fix CW

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const listUl = document.getElementById("list-ul");
+const imageElement = document.getElementById("foto");
+const nameElement = document.getElementById("name");
+const infoElement = document.getElementById("info");
+const addressElement = document.getElementById("address");
+const phoneElement = document.getElementById("phone");
 
 async function getContact() {
   const response = await fetch(
     "https://weizen-w.github.io/lesson_14_address_book/adress_book.json"
   );
   const obj = await response.json();
-  const { firstName, lastName, gender, age, address, phoneNumbers } = obj;
+  const { foto, firstName, lastName, gender, age, address, phoneNumbers } = obj;
   const { streetAddress, city, state, postalCode } = address;
   const { type, number } = phoneNumbers;
-  const contact = document.createElement("li");
-  contact.textContent = `${firstName} ${lastName}; age: ${age}, gender: ${gender}; adress: ${state} - ${postalCode}, ${city}, ${streetAddress} ; phone: ${number} - ${type} `;
-  listUl.append(contact);
+  imageElement.src = foto;
+  nameElement.textContent = `${firstName} ${lastName}`;
+  infoElement.textContent = `age: ${age}, gender: ${gender}`;
+  addressElement.textContent = `adress: ${state} - ${postalCode}, ${city}, ${streetAddress}`
+  phoneElement.textContent = `phone: ${number} - ${type}`;
 }
 
 getContact();
